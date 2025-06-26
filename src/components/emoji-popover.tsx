12f8ef7571e40d2a8576de57595402699270335d
Arshak Asriyan <aasriyan01@gmail.com>
2025-06-25 21:05:12 -0500

    Add emoji support and image handling in editor component
    - Integrated `@emoji-mart/data` and `@emoji-mart/react` for emoji selection in the editor.
    - Created `EmojiPopover` component to manage emoji selection and display.
    - Enhanced `Editor` component to handle image uploads and display selected images.
    - Updated UI to include functionality for removing uploaded images and improved tooltip hints.

---
+++ src/components/emoji-popover.tsx
@@ -0,0 +1,60 @@
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+
+import { useState } from "react";
+
+interface EmojiPopoverProps {
+  children: React.ReactNode;
+  hint?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onEmojiSelect: (emoji: any) => void;
+}
+
+export const EmojiPopover = ({
+  children,
+  hint = "Emoji",
+  onEmojiSelect,
+}: EmojiPopoverProps) => {
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSelect = (emoji: any) => {
+    onEmojiSelect(emoji);
+    setPopoverOpen(false);
+
+    setTimeout(() => {
+      setTooltipOpen(false);
+    }, 500);
+  };
+
+  return (
+    <TooltipProvider>
+      <Popover open={popoverOpen} onOpenChange={setPopoverOpen}>
+        <Tooltip
+          open={tooltipOpen}
+          onOpenChange={setTooltipOpen}
+          delayDuration={50}
+        >
+          <PopoverTrigger asChild>
+            <TooltipTrigger asChild>{children}</TooltipTrigger>
+          </PopoverTrigger>
+          <TooltipContent className="bg-black text-white border border-white/5">
+            <p className="font-medium text-xs">{hint}</p>
+          </TooltipContent>
+        </Tooltip>
+        <PopoverContent className="w-full p-0 border-none shadow-none">
+          <Picker data={data} onEmojiSelect={onSelect} />
+        </PopoverContent>
+      </Popover>
+    </TooltipProvider>
+  );
+};
