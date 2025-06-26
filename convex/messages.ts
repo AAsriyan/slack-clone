d90f9d81f5d7f96c079409107a55b2e388076595
Arshak Asriyan <aasriyan01@gmail.com>
2025-06-26 13:35:14 -0500

    Add messaging and upload functionality
    - Introduced `createMessage` mutation for sending messages with optional images, linked to workspaces, channels, and conversations.
    - Added `messages` table schema to store message data, including body, image, and metadata.
    - Implemented `generateUploadUrl` mutation for handling image uploads.
    - Updated `ChatInput` and `Editor` components to support message creation and image handling.
    - Created hooks for managing message creation and upload URL generation, enhancing user experience in chat interactions.

---
+++ convex/messages.ts
@@ -0,0 +1,76 @@
+import { Id } from "./_generated/dataModel";
+import { mutation, QueryCtx } from "./_generated/server";
+import { v } from "convex/values";
+import { getAuthUserId } from "@convex-dev/auth/server";
+
+const getMember = async (
+  ctx: QueryCtx,
+  workspaceId: Id<"workspaces">,
+  userId: Id<"users">
+) => {
+  return await ctx.db
+    .query("members")
+    .withIndex("by_workspace_id_user_id", (q) =>
+      q.eq("workspaceId", workspaceId).eq("userId", userId)
+    )
+    .unique();
+};
+
+export const createMessage = mutation({
+  args: {
+    body: v.string(),
+    image: v.optional(v.id("_storage")),
+    workspaceId: v.id("workspaces"),
+    channelId: v.optional(v.id("channels")),
+    parentMessageId: v.optional(v.id("messages")),
+    conversationId: v.optional(v.id("conversations")),
+  },
+  handler: async (ctx, args) => {
+    const {
+      body,
+      image,
+      workspaceId,
+      channelId,
+      conversationId,
+      parentMessageId,
+    } = args;
+
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const member = await getMember(ctx, workspaceId, userId);
+
+    if (!member) {
+      throw new Error("Unauthorized");
+    }
+
+    let _conversationId = conversationId;
+
+    // Only possible if we are replying in a thread and not in a channel
+    if (!conversationId && !channelId && parentMessageId) {
+      const parentMessage = await ctx.db.get(parentMessageId);
+
+      if (!parentMessage) {
+        throw new Error("Parent message not found");
+      }
+
+      _conversationId = parentMessage.conversationId;
+    }
+
+    const messageId = await ctx.db.insert("messages", {
+      body,
+      image,
+      memberId: member._id,
+      workspaceId,
+      channelId,
+      conversationId: _conversationId,
+      parentMessageId,
+      updatedAt: Date.now(),
+    });
+
+    return messageId;
+  },
+});
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -77,7 +77,17 @@ const Editor = ({
             enter: {
               key: "Enter",
               handler: () => {
-                // TODO: Submit form
+                const text = quill.getText();
+                const addedImage = imageElementRef.current?.files?.[0] || null;
+
+                const isEmpty =
+                  !addedImage &&
+                  text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+
+                if (isEmpty) return;
+                const body = JSON.stringify(quill.getContents());
+
+                submitRef.current?.({ body, image: addedImage });
                 return;
               },
             },
@@ -134,7 +144,7 @@ const Editor = ({
     quill.insertText(quill.getSelection()?.index || 0, emoji.native);
   };
 
-  const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+  const isEmpty = !image && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   return (
     <div className="flex flex-col">
@@ -145,7 +155,12 @@ const Editor = ({
         className="hidden"
         onChange={(e) => setImage(e.target.files?.[0] || null)}
       />
-      <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within: shadow-sm transition bg-white">
+      <div
+        className={cn(
+          "flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within: shadow-sm transition bg-white",
+          disabled && "opacity-50"
+        )}
+      >
         <div ref={containerRef} className="h-full ql-custom" />
         {!!image && (
           <div className="p-2">
@@ -207,7 +222,7 @@ const Editor = ({
                   disabled={disabled}
                   size="sm"
                   className="bg-[#007A5A] hover:bg-[#007A5A]/80 text-white"
-                  onClick={() => {}}
+                  onClick={onCancel}
                 >
                   Cancel
                 </Button>
@@ -215,7 +230,12 @@ const Editor = ({
                   disabled={disabled || isEmpty}
                   size="sm"
                   variant="ghost"
-                  onClick={() => {}}
+                  onClick={() =>
+                    onSubmit({
+                      body: JSON.stringify(quillRef.current?.getContents()),
+                      image,
+                    })
+                  }
                 >
                   Save
                 </Button>
@@ -233,7 +253,12 @@ const Editor = ({
                     ? "bg-white hover:bg-white text-muted-foreground"
                     : "bg-[#007A5A] hover:bg-[#007A5A]/80 text-white"
                 )}
-                onClick={() => {}}
+                onClick={() =>
+                  onSubmit({
+                    body: JSON.stringify(quillRef.current?.getContents()),
+                    image,
+                  })
+                }
               >
                 <MdSend className="size-4" />
               </Button>
